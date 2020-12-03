ac600204c0f9176836e985ee60b75ea4e7eb4979
Zewujun <62692045+Zewujun@users.noreply.github.com>
2020-12-03 13:32:20 +0300

    Update laboratornay.py

--- laboratornay.py
+++ laboratornay.py
@@ -10,12 +10,12 @@ def data():
 
 def div_c(gx, mx):
     r = len(bin(gx)) - 1
-    if len(bin(gx)) < len(bin(mx)):
+    if len(bin(mx)) < len(bin(gx)):
         return mx
     tmp = gx << len(bin(mx)) - len(bin(gx))
     cx = mx ^ tmp
     while len(bin(cx)) >= len(bin(gx)):
-        tmp = gx << len(bin(cx)) - len(bin(mx))
+        tmp = gx << len(bin(cx)) - len(bin(gx))
         cx = cx ^ gx
         print('c ='.format(bin(cx)))
     return cx
@@ -25,17 +25,17 @@ def binaryGenerator(l, r):
     e = []
     n = l + r
     for i in range(0, n):
-        e = random.randint(0, 100)
+        e = random.randint(16, 31)
     return e
 
 
 def div_s(b, gx):
-    if len(bin(gx)) < len(bin(b)):
+    if len(bin(b)) < len(bin(gx)):
         return b
     tmp = gx << len(bin(b)) - len(bin(gx))
     sx = b ^ tmp
     while len(bin(sx)) >= len(bin(gx)):
-        tmp = gx << len(bin(sx)) - len(bin(b))
+        tmp = gx << len(bin(sx)) - len(bin(gx))
         sx = sx ^ gx
     return sx
 
@@ -52,13 +52,14 @@ def main():
     l, k, m = data()
     r = len(str(gx)) - 1
     mx = m << r
-    print("{0:b}".format(mx))
+    print('m(x) = ', "{0:b}".format(mx))
     cx = div_c(gx, mx)
     ax = (mx << r) + cx
-    print("{0:b}".format(ax))
+    print('a(x) = ',"{0:b}".format(ax))
     e = binaryGenerator(l, r)
-    print("{0:b}".format(e))
+    print('e(x) = ',"{0:b}".format(e))
     b = ax ^ e
+    print('b(x) = ', "{0:b}".format(b))
     sx = div_s(b, gx)
     if sx == 0 and e != 0:
         print('Ошибок не обнаружено')
